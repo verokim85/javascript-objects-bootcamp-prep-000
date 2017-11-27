d65df4a557d2e1fb80b00bdb2bc5a3584c9df918
Veronica Kim <vero.a.kim@gmail.com>
2017-11-27 00:04:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist =  new Object({artist names: 'song titles'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  var playlist = new Object
+  var playlist = new Object({})
 }
