a575e5535b8739a97475cdc715fe507c46c54aa9
Veronica Kim <vero.a.kim@gmail.com>
2017-11-27 00:39:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,5 @@ return Object.assign({}, playlist, {[artistName]: songTitle});
 
 function removeFromPlaylist(playlist, artistName) {
  delete playlist.artistName;
-  return removeFromPlaylist;
+ playlist;
 }
