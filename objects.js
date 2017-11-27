28ee9f26aadfeb17715453a1f432fe294b63b86b
Veronica Kim <vero.a.kim@gmail.com>
2017-11-27 00:19:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist =  new Object({artistName: 'songTitle'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-return Object.assign({}, artistName, songTitle)
+var playlist = Object.assign({artistName: 'Phil Ochs'}, {songTitle: 'Here"s to the State of Mississipi'});
 }
